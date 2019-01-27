d09033f9c45878ae003f861b81f74e90b643f341
sbx_user1059 <sbx_user1059@ip-10-129-81-65.eu-west-2.compute.internal>
2019-01-27 10:11:18 +0000

    Sun Jan 27 10:11:18 UTC 2019

--- src/main/java/befaster/solutions/CHK/Item.java
+++ src/main/java/befaster/solutions/CHK/Item.java
@@ -1,4 +1,21 @@
 package befaster.solutions.CHK;
 
 public class Item {
+
+    private final String sku;
+    private final int Price;
+
+    public Item(String sku, int price) {
+        this.sku = sku;
+        Price = price;
+    }
+
+    public String getSku() {
+        return sku;
+    }
+
+    public int getPrice() {
+        return Price;
+    }
 }
+
