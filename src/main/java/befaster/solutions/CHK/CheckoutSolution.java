e1ed6a90d619f14037c9b053fa443d1d2aba062d
sbx_user1059 <sbx_user1059@ip-10-129-81-65.eu-west-2.compute.internal>
2019-01-27 11:08:18 +0000

    Sun Jan 27 11:08:18 UTC 2019

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -22,5 +22,3 @@ public class CheckoutSolution {
         return itemFactory.getTotalPrice(items);
     }
 }
-
-
--- src/main/java/befaster/solutions/CHK/Discount.java
+++ src/main/java/befaster/solutions/CHK/Discount.java
@@ -19,4 +19,3 @@ public class Discount {
         return descountedPrice;
     }
 }
-
