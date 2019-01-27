0a4ba17c9453e05693e5ce1c67ff7898b8f600d8
sbx_user1059 <sbx_user1059@ip-10-129-81-65.eu-west-2.compute.internal>
2019-01-27 09:53:18 +0000

    Sun Jan 27 09:53:18 UTC 2019

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -4,6 +4,6 @@ import befaster.runner.SolutionNotImplementedException;
 
 public class CheckoutSolution {
     public Integer checkout(String skus) {
-        throw new SolutionNotImplementedException();
+        return 50;
     }
 }
--- src/test/java/befaster/solutions/CHK/CheckoutSolutionTest.java
+++ src/test/java/befaster/solutions/CHK/CheckoutSolutionTest.java
@@ -1,5 +1,26 @@
+package befaster.solutions.CHK;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
 public class CheckoutSolutionTest {
 
+    private CheckoutSolution checkoutSolution = new CheckoutSolution();
+
+    @Test
+    public void checkout_shouldReturnPriceOfItemA() {
+
+        Integer result = checkoutSolution.checkout("A");
+
+        assertThat(result, is(50));
+    }
+
+    @Test
+    public void checkout_shouldReturnPriceOfItemB() {
+        Integer result = checkoutSolution.checkout("B");
+
+        assertThat(result, is(30));
+    }
 }
